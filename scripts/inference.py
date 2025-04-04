51aff9c3c224ee8276eb36f4dd6c9da32216f13f
Ivan Vykopal <ivan.vykopal@gmail.com>
2025-04-04 06:28:23 +0000

    feat: add baseline

--- scripts/inference.py
+++ scripts/inference.py
@@ -1,14 +1,17 @@
 import argparse
 from src.models import model_factory
 from src.prompts import prompt_factory  
+from src.evaluation import evaluate_outputs
 import os
 import pandas as pd
 import yaml
+import wandb
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', type=str, help='Path to config')
     parser.add_argument('--data_path', type=str, help='Path to data')
+    parser.add_argument('--prompt_name', type=str, help='Prompt name')
     
     args = parser.parse_args()
     return args
@@ -19,6 +22,17 @@ def main():
     # load config
     with open(args.config, 'r') as f:
         config = yaml.safe_load(f)
+        
+    # initialize wandb
+    wandb.init(
+        project='clef2025',
+        config=config
+    )
+    
+    # name the wandb run with the model name prompt name and language
+    # get the language from ./data/train/train-{language}.csv data path
+    language = args.data_path.split('/')[-1].split('-')[-1].replace('.csv', '')
+    wandb.run.name = f"{config['model']['name'].split('/')[-1]}_{args.prompt_name}_{language}"
         
     model = model_factory(
         **config['model']
@@ -39,11 +53,32 @@ def main():
     
     # Save generated outputs
     df['generated_output'] = generated_outputs
+    df['prompt'] = posts
     
     output_path = args.data_path.split('/')[-1].replace('.csv', '_generated.csv')
     model_name = config['model']['name'].split('/')[-1]
     os.makedirs(f'./results/{model_name}', exist_ok=True)
-    df = df.to_csv(f'./results/{model_name}/{output_path}', index=False)
+    os.makedirs(f'./results/{model_name}/{args.prompt_name}', exist_ok=True)
+    df.to_csv(f'./results/{model_name}/{args.prompt_name}/{output_path}', index=False)
+    
+    # save dataframe to wandb
+    wandb.log({'generated_outputs': wandb.Table(dataframe=df)})
+    
+    # evaluate the results
+    metrics = evaluate_outputs(
+        df['generated_output'].tolist(),
+        df['normalized claim'].tolist()
+    )
+    
+    # log metrics
+    wandb.log(metrics)
+    
+    print(metrics)
+    # save metrics
+    with open(f'./results/{model_name}/{args.prompt_name}/metrics.txt', 'w') as f:
+        f.write(str(metrics))
+        
+    wandb.finish()
     
 
 if __name__ == '__main__':
--- src/evaluation/evaluation.py
+++ src/evaluation/evaluation.py
@@ -1,6 +1,17 @@
 # use meteor metric to evaluate the generated outputs
-from evaluate import evaluate
+import nltk
+nltk.download('punkt')
+from nltk.tokenize import RegexpTokenizer
+import evaluate
+import numpy as np
 
 def evaluate_outputs(predictions, references):
     meteor = evaluate.load('meteor')
-    return meteor.compute(predictions=predictions, references=references)
+    text_preds = [(p if str(p).endswith(("!", "！", "?", "？", "。")) else str(p) + "。") for p in predictions]
+    text_labels = [(l if l.endswith(("!", "！", "?", "？", "。")) else l + "。") for l in references]
+    sent_tokenizer_jp = RegexpTokenizer(u'[^!！?？。]*[!！?？。]')
+    
+    text_preds = ["\n".join(np.char.strip(sent_tokenizer_jp.tokenize(p))) for p in text_preds]
+    text_labels = ["\n".join(np.char.strip(sent_tokenizer_jp.tokenize(l))) for l in text_labels]
+    
+    return meteor.compute(predictions=text_preds, references=text_labels)
